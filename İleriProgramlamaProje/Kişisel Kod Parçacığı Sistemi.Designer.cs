f811f546d99c6a596a91b3ba67189053857ed0a6
90531 <90531@DESKTOP-U35RIOT>
2025-12-29 12:13:38 +0300

    Proje dosyası ekle.

---
+++ İleriProgramlamaProje/Kişisel Kod Parçacığı Sistemi.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace İleriProgramlamaProje
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.lstSnippets = new System.Windows.Forms.ListBox();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.cmbLanguage = new System.Windows.Forms.ComboBox();
+            this.rtbCode = new System.Windows.Forms.RichTextBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.Aqua;
+            this.btnSave.Font = new System.Drawing.Font("Arial Narrow", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSave.Location = new System.Drawing.Point(800, 133);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(177, 52);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Kaydet";
+            this.btnSave.UseVisualStyleBackColor = false;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Red;
+            this.btnDelete.Font = new System.Drawing.Font("Arial Narrow", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnDelete.ForeColor = System.Drawing.Color.FloralWhite;
+            this.btnDelete.Location = new System.Drawing.Point(800, 392);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(177, 52);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "Sil";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.BackColor = System.Drawing.Color.Aqua;
+            this.btnCopy.Font = new System.Drawing.Font("Arial Narrow", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCopy.Location = new System.Drawing.Point(800, 214);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(177, 52);
+            this.btnCopy.TabIndex = 2;
+            this.btnCopy.Text = "Kopyala";
+            this.btnCopy.UseVisualStyleBackColor = false;
+            // 
+            // lstSnippets
+            // 
+            this.lstSnippets.FormattingEnabled = true;
+            this.lstSnippets.ItemHeight = 16;
+            this.lstSnippets.Location = new System.Drawing.Point(3, 50);
+            this.lstSnippets.Name = "lstSnippets";
+            this.lstSnippets.Size = new System.Drawing.Size(151, 292);
+            this.lstSnippets.TabIndex = 3;
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(800, 36);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(177, 22);
+            this.txtTitle.TabIndex = 4;
+            this.txtTitle.Text = "Başlık";
+            // 
+            // cmbLanguage
+            // 
+            this.cmbLanguage.FormattingEnabled = true;
+            this.cmbLanguage.Location = new System.Drawing.Point(800, 64);
+            this.cmbLanguage.Name = "cmbLanguage";
+            this.cmbLanguage.Size = new System.Drawing.Size(177, 24);
+            this.cmbLanguage.TabIndex = 5;
+            this.cmbLanguage.Text = "Dil seç";
+            // 
+            // rtbCode
+            // 
+            this.rtbCode.Location = new System.Drawing.Point(180, 83);
+            this.rtbCode.Name = "rtbCode";
+            this.rtbCode.Size = new System.Drawing.Size(538, 361);
+            this.rtbCode.TabIndex = 6;
+            this.rtbCode.Text = "";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(3, 22);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(151, 22);
+            this.txtSearch.TabIndex = 7;
+            this.txtSearch.Text = "Ara";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(177, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Kodu aşağı yazınız";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.Aqua;
+            this.btnUpdate.Font = new System.Drawing.Font("Arial Narrow", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnUpdate.Location = new System.Drawing.Point(800, 290);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(177, 52);
+            this.btnUpdate.TabIndex = 9;
+            this.btnUpdate.Text = "Güncelle";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(1030, 482);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.rtbCode);
+            this.Controls.Add(this.cmbLanguage);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.lstSnippets);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSave);
+            this.Name = "Form1";
+            this.Text = "SnippetVault";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.ListBox lstSnippets;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.ComboBox cmbLanguage;
+        private System.Windows.Forms.RichTextBox rtbCode;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
+
---
+++ İleriProgramlamaProje/Kişisel Kod Parçacığı Sistemi.cs
@@ -0,0 +1,417 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Drawing;
+
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
+
+namespace İleriProgramlamaProje
+{
+   
+    public partial class Form1 : Form
+    {
+        // Veritabanı yöneticisi
+        DatabaseManager db = new DatabaseManager();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ListeyiDoldur();
+            if (cmbLanguage.Items.Count > 0) cmbLanguage.SelectedIndex = 0;
+            cmbLanguage.Items.Clear(); 
+            cmbLanguage.Items.AddRange(new object[] {
+        "C#",
+        "Python",
+        "SQL",
+        "Java",
+        "JavaScript",
+        "C++",
+        "HTML/CSS",
+        "PHP",
+        "C"
+          });
+
+            
+            btnSave.Click += BtnSave_Click;
+            btnDelete.Click += BtnDelete_Click;
+            btnCopy.Click += BtnCopy_Click;
+            txtSearch.TextChanged += TxtSearch_TextChanged;
+            lstSnippets.SelectedIndexChanged += LstSnippets_SelectedIndexChanged;
+            rtbCode.TextChanged += RtbCode_TextChanged;
+            SetPlaceholder(txtTitle, "Başlık Girin");
+            SetPlaceholder(txtSearch, "Ara");
+            SetPlaceholder(cmbLanguage, "Dil seçiniz");
+            btnUpdate.Click += BtnUpdate_Click;
+        }
+
+        private void ListeyiDoldur()
+        {
+            lstSnippets.Items.Clear();
+            var snippets = db.GetAllSnippets();
+            foreach (var s in snippets)
+            {
+                lstSnippets.Items.Add(s);
+            }
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            
+            if (txtTitle.Text == "Başlık Girin..." || string.IsNullOrWhiteSpace(rtbCode.Text))
+            {
+                MessageBox.Show("Lütfen başlık ve kod alanını doldurun.");
+                return;
+            }
+
+            Snippet yeniSnippet = new Snippet
+            {
+                Title = txtTitle.Text,
+                CodeContent = rtbCode.Text,
+                Language = cmbLanguage.Text
+            };
+
+            db.AddSnippet(yeniSnippet);
+            ListeyiDoldur();
+            MessageBox.Show("Kaydedildi!");
+            txtTitle.Clear();
+            rtbCode.Clear();
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            if (lstSnippets.SelectedItem == null) return;
+
+            Snippet secilen = (Snippet)lstSnippets.SelectedItem;
+            DialogResult cevap = MessageBox.Show("Silinsin mi?", "Onay", MessageBoxButtons.YesNo);
+
+            if (cevap == DialogResult.Yes)
+            {
+                db.DeleteSnippet(secilen.Id);
+                ListeyiDoldur();
+                txtTitle.Clear();
+                rtbCode.Clear();
+            }
+        }
+
+        private void BtnCopy_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(rtbCode.Text))
+            {
+                Clipboard.SetText(rtbCode.Text);
+                MessageBox.Show("Kopyalandı!");
+            }
+        }
+
+        private void TxtSearch_TextChanged(object sender, EventArgs e)
+        {
+            
+            string aranan = txtSearch.Text;
+
+            lstSnippets.Items.Clear();
+
+            List<Snippet> sonuclar;
+
+            if (string.IsNullOrWhiteSpace(aranan))
+            {
+                sonuclar = db.GetAllSnippets(); // Boşsa hepsini getir
+            }
+            else
+            {
+                sonuclar = db.SearchSnippets(aranan); // Doluysa ara
+            }
+
+            // Sonuçları listeye ekle
+            foreach (var s in sonuclar)
+            {
+                lstSnippets.Items.Add(s);
+            }
+        }
+
+        private void LstSnippets_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstSnippets.SelectedItem == null) return;
+
+            Snippet secilen = (Snippet)lstSnippets.SelectedItem;
+            txtTitle.Text = secilen.Title;
+            rtbCode.Text = secilen.CodeContent;
+            cmbLanguage.Text = secilen.Language;
+            Renklendir();
+        }
+
+        
+
+
+private void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            
+            if (lstSnippets.SelectedItem == null)
+            {
+                MessageBox.Show("Lütfen güncellenecek kaydı listeden seçin.");
+                return;
+            }
+
+            
+            if (txtTitle.Text == "Başlık Girin..." || string.IsNullOrWhiteSpace(txtTitle.Text))
+            {
+                MessageBox.Show("Lütfen geçerli bir başlık girin.");
+                return;
+            }
+
+            
+            Snippet secilenEskiVeri = (Snippet)lstSnippets.SelectedItem;
+
+           
+            Snippet guncelVeri = new Snippet
+            {
+                Id = secilenEskiVeri.Id, 
+                Title = txtTitle.Text,
+                CodeContent = rtbCode.Text,
+                Language = cmbLanguage.Text
+            };
+
+            
+            db.UpdateSnippet(guncelVeri);
+
+            
+            ListeyiDoldur();
+            MessageBox.Show("Kayıt başarıyla güncellendi!");
+
+            
+            txtTitle.Text = "Başlık Girin..."; 
+            txtTitle.ForeColor = Color.Gray;
+            rtbCode.Clear();
+        }
+
+
+        private void RtbCode_TextChanged(object sender, EventArgs e)
+        {
+            Renklendir();
+        }
+
+        private void Renklendir()
+        {
+            int cursorIndex = rtbCode.SelectionStart;
+            rtbCode.SelectAll();
+            rtbCode.SelectionColor = Color.Black;
+            rtbCode.SelectionFont = new Font("Consolas", 10, FontStyle.Regular);
+
+            string keywords = @"\b(public|private|protected|class|void|int|string|return|if|else|for|while|new|using)\b";
+            Regex rex = new Regex(keywords);
+
+            foreach (Match m in rex.Matches(rtbCode.Text))
+            {
+                rtbCode.Select(m.Index, m.Length);
+                rtbCode.SelectionColor = Color.Blue;
+                rtbCode.SelectionFont = new Font("Consolas", 10, FontStyle.Bold);
+            }
+
+            rtbCode.SelectionLength = 0;
+            rtbCode.SelectionStart = cursorIndex;
+            rtbCode.SelectionColor = Color.Black;
+        }
+        
+        private void SetPlaceholder(Control kutu, string yerTutucuMetni)
+        {
+            
+            kutu.Text = yerTutucuMetni;
+            kutu.ForeColor = Color.Black;
+
+            
+            kutu.Enter += (sender, e) =>
+            {
+                if (kutu.Text == yerTutucuMetni)
+                {
+                    kutu.Text = "";
+                    kutu.ForeColor = Color.Black;
+                }
+            };
+
+            
+            kutu.Leave += (sender, e) =>
+            {
+                if (string.IsNullOrWhiteSpace(kutu.Text))
+                {
+                    kutu.Text = yerTutucuMetni;
+                    kutu.ForeColor = Color.Black;
+                }
+            };
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    
+
+    public class Snippet
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string CodeContent { get; set; }
+        public string Language { get; set; }
+
+        public override string ToString()
+        {
+            return $"[{Language}] {Title}";
+        }
+    }
+
+    public class DatabaseManager
+    {
+        private string dbFile = "MySnippets.db";
+        private string connectionString;
+
+        public DatabaseManager()
+        {
+            connectionString = $"Data Source={dbFile};Version=3;";
+
+            // Dosya yoksa oluştur (Boş dosya oluşturur)
+
+
+            if (!File.Exists(dbFile))
+            {
+                SQLiteConnection.CreateFile(dbFile);
+            }
+
+            
+            using (var conn = new SQLiteConnection(connectionString))
+            {
+                conn.Open();
+                string sql = @"
+            CREATE TABLE IF NOT EXISTS Snippets (
+                Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                Title TEXT,
+                CodeContent TEXT,
+                Language TEXT
+            )";
+                using (var cmd = new SQLiteCommand(sql, conn))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void AddSnippet(Snippet s)
+        {
+            using (var conn = new SQLiteConnection(connectionString))
+            {
+                conn.Open();
+                string sql = "INSERT INTO Snippets (Title, CodeContent, Language) VALUES (@t, @c, @l)";
+                using (var cmd = new SQLiteCommand(sql, conn))
+                {
+                    cmd.Parameters.AddWithValue("@t", s.Title);
+                    cmd.Parameters.AddWithValue("@c", s.CodeContent);
+                    cmd.Parameters.AddWithValue("@l", s.Language);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void DeleteSnippet(int id)
+        {
+            using (var conn = new SQLiteConnection(connectionString))
+            {
+                conn.Open();
+                string sql = "DELETE FROM Snippets WHERE Id = @id";
+                using (var cmd = new SQLiteCommand(sql, conn))
+                {
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public List<Snippet> GetAllSnippets()
+        {
+            var list = new List<Snippet>();
+            using (var conn = new SQLiteConnection(connectionString))
+            {
+                conn.Open();
+                string sql = "SELECT * FROM Snippets ORDER BY Id DESC";
+                using (var cmd = new SQLiteCommand(sql, conn))
+                {
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            list.Add(new Snippet
+                            {
+                                Id = Convert.ToInt32(reader["Id"]),
+                                Title = reader["Title"].ToString(),
+                                CodeContent = reader["CodeContent"].ToString(),
+                                Language = reader["Language"].ToString()
+                            });
+                        }
+                    }
+                }
+            }
+            return list;
+        }
+        
+
+        public List<Snippet> SearchSnippets(string keyword)
+        {
+            var list = new List<Snippet>();
+            using (var conn = new SQLiteConnection(connectionString))
+            {
+                conn.Open();
+               
+                string sql = "SELECT * FROM Snippets WHERE Title LIKE @k OR CodeContent LIKE @k OR Language LIKE @k ORDER BY Id DESC";
+
+                using (var cmd = new SQLiteCommand(sql, conn))
+                {
+                    
+                    cmd.Parameters.AddWithValue("@k", "%" + keyword + "%");
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            list.Add(new Snippet
+                            {
+                                Id = Convert.ToInt32(reader["Id"]),
+                                Title = reader["Title"].ToString(),
+                                CodeContent = reader["CodeContent"].ToString(),
+                                Language = reader["Language"].ToString()
+                            });
+                        }
+                    }
+                }
+            }
+            return list;
+        }
+
+        
+        public void UpdateSnippet(Snippet s)
+        {
+            using (var conn = new SQLiteConnection(connectionString))
+            {
+                conn.Open();
+                
+                string sql = "UPDATE Snippets SET Title=@t, CodeContent=@c, Language=@l WHERE Id=@id";
+                using (var cmd = new SQLiteCommand(sql, conn))
+                {
+                    cmd.Parameters.AddWithValue("@t", s.Title);
+                    cmd.Parameters.AddWithValue("@c", s.CodeContent);
+                    cmd.Parameters.AddWithValue("@l", s.Language);
+                    cmd.Parameters.AddWithValue("@id", s.Id);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
